892da65136eed07ce13af0f81a4c704ba6e50eaa
Nicu Siderias <nicu098@gmail.com>
2026-03-23 21:53:42 +0200

    Add tests, Trivy scanning, and updated CI pipeline

--- src/server/index.js
+++ src/server/index.js
@@ -6,4 +6,8 @@ const app = express();
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+}
+
+module.exports = app;
